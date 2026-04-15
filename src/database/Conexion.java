d61a2dff2228bcb74900be20e082e145fa97e359
Jjose17 <chiquibar2007rgmail.com>
2026-04-15 17:18:17 -0500

    Refractor: Implementacion del patron singleton en la clase de Conexion

--- src/database/Conexion.java
+++ src/database/Conexion.java
@@ -15,9 +15,10 @@ public class Conexion {
     private final String PASSWORD = "";
     
     public Connection cadena;
+    public static Conexion instancia;
     
     
-    public Conexion(){
+    private Conexion(){
         this.cadena = null;
     }
     
@@ -40,6 +41,11 @@ public class Conexion {
         }
     }
 
-
+public static Conexion getInstancia(){
+    if(instancia == null){
+        instancia = new Conexion();
+    }
+    return instancia;
+}
     
 }
