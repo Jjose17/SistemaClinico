f395c3887e8cbe603d201869247882adeb9528d4
Jjose17 <chiquibar2007rgmail.com>
2026-04-20 11:16:04 -0500

    Feat Medico y Secretaria: Creamos la clase medico y secretaria, sin validaciones

---
+++ src/entidades/Medico.java
@@ -0,0 +1,48 @@
+
+package entidades;
+
+
+public class Medico extends Persona{
+    
+    private String especialidad;
+    private String numLicencia;
+    private String horario;
+    
+    public Medico(){
+        
+    }
+    public Medico(int id, String nombre, String apellido, String documento, String telefono, String correo,
+            boolean activo, String especialidad, String numLicencia, String horario){
+        super(id, nombre, apellido,documento, telefono, correo, activo);
+        this.especialidad = especialidad;
+        this.horario = horario;
+        this.numLicencia=numLicencia;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getNumLicencia() {
+        return numLicencia;
+    }
+
+    public void setNumLicencia(String numLicencia) {
+        this.numLicencia = numLicencia;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+    
+    
+    
+}
---
+++ src/entidades/Secretaria.java
@@ -0,0 +1,28 @@
+
+package entidades;
+
+public class Secretaria extends Persona{
+    
+    private String turno;
+    
+    public Secretaria(){
+        
+    }
+    
+    public Secretaria(int id, String nombre, String apellido, String documento, String telefono,
+            String correo, boolean activo, String turno){
+        super(id, nombre, apellido, telefono, documento, correo, activo);
+        
+        this.turno=turno;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+    
+    
+}
