f3e1e3ba167f2a92534bbf75d2da2a89a1e5e31d
Jjose17 <chiquibar2007rgmail.com>
2026-04-20 11:07:22 -0500

    Feat Paciente: Creamos la clase paciente, con una pequeña validacion en la parte de edad

---
+++ src/entidades/Paciente.java
@@ -0,0 +1,63 @@
+
+package entidades;
+
+public class Paciente extends Persona{
+    
+    private int edad;
+    private String direccion;
+    private String historialMedico;
+    private Integer prepagadaID; //Con esto podremos conectar la prepagada con el paciente que la tenga 
+    
+    public Paciente(){
+        
+    }
+    public Paciente(int id, String nombre, String apellido, String documento,
+            String telefono, String correo, boolean activo, int edad,
+            String direccion, String historialMedico, Integer prepagadaID ){
+        
+        
+        //Estos son los atributos propios de esta clase
+        super(id, nombre, apellido, documento, telefono, correo, activo);
+        this.edad = edad;
+        this.direccion = direccion;
+        this.historialMedico = historialMedico;
+        this.prepagadaID = prepagadaID;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        if(edad < 0){
+            throw new IllegalArgumentException("La edad no puede ser menor a 0");
+        }
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getHistorialMedico() {
+        return historialMedico;
+    }
+
+    public void setHistorialMedico(String historialMedico) {
+        this.historialMedico = historialMedico;
+    }
+
+    public Integer getPrepagadaID() {
+        return prepagadaID;
+    }
+
+    public void setPrepagadaID(Integer prepagadaID) {
+        this.prepagadaID = prepagadaID;
+    }
+    
+    
+}
+
