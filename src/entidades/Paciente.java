614f15efba8de479c38ea60dc059c93168efc852
Jjose17 <chiquibar2007rgmail.com>
2026-05-16 20:29:51 -0500

    Fix Entidad Paciente y PacienteDAO: Arregle un problema, Paciente me aparecio en Datos, entonces lo borre

--- src/entidades/Paciente.java
+++ src/entidades/Paciente.java
@@ -1,29 +1,14 @@
 package entidades;
 
-public class Paciente extends Persona {
-
+public class Paciente extends Persona{
+    
     private int edad;
     private String direccion;
     private String historialMedico;
-<<<<<<< HEAD
-    private Integer prepagadaID;
-
-    public Paciente() {
-    }
-
-    public Paciente(int id, String nombre, String apellido, String documento, String telefono, String correo,
-            boolean activo, int edad, String direccion, String historialMedico, Integer prepagadaID) {
-=======
     private Integer prepagadaID; //Con esto podremos conectar la prepagada con el paciente que la tenga 
     
     public Paciente(int aInt, String string, String string1, String string2, String string3, String string4, boolean aBoolean, int aInt1, String string5, String string6, Integer par){
->>>>>>> datos
         
-        super(id, nombre, apellido, documento, telefono, correo, activo);
-        this.edad = edad;
-        this.direccion = direccion;
-        this.historialMedico = historialMedico;
-        this.prepagadaID = prepagadaID;
     }
 
     public int getEdad() {
@@ -60,17 +45,12 @@ public class Paciente extends Persona {
 
     @Override
     public String toString() {
-        return "Paciente{" + "id=" + getId() + 
-                ", nombre=" + getNombre() + 
-                ", apellido=" + getApellido()
-                + ", documento=" + getDocumento() + 
-                ", telefono=" + getTelefono() + 
-                ", correo=" + getCorreo()
-                + ", activo=" + isActivo() + 
-                ", edad=" + edad + 
-                ", direccion=" + direccion
-                + ", historialMedico=" + historialMedico + 
-                ", prepagadaID=" + prepagadaID + '}';
+        return "Paciente: " + getNombre() + "edad=" + getEdad() + ", direccion=" + direccion + ", historialMedico=" + historialMedico + ", prepagadaID=" + prepagadaID + '}';
     }
-}
 
+
+ 
+    
+    
+    
+}
